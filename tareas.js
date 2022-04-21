478cf6ea64e9996a73e6b50a6c3936eaa405ec88
Manuel Alvarez <mjalvarezx@alumno.uspg.edu.gt>
2022-04-21 15:37:32 -0600

    carga err y cambios en JS

--- tareas.js
+++ tareas.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var _priorid = document.getElementById("prioridad").value;
         
         //inserta el valor de las filas, al final agrega un cuadro de selección para determinar el estado de la tarea
-        var fila="<tr><td>"+_tarea+"</td><td>"+_descrip+"</td><td class='' type='number'>"+_categ+"</td><td id='prior_tar' type='number' class='text-right' style='color: white;'>"+_priorid+"</td></tr>" +" <td class='text-right'><select class='form-select' aria-label='Default select example' name='estado' id='estado'><option selected>elige una opción.....</option><option value='pendiente'>Pendiente</option><option value='completado'>Completado</option><option value='cancelado'>Cancelado</option></select></td>";
+        var fila="<tr><td>"+_tarea+"</td><td>"+_descrip+"</td><td class='' type='number'>"+_categ+"</td><td id='prior_tar' type='number' class='text-right' style='color: white;'>"+_priorid+"</td></tr>" +" <td class='text-right'><select class='form-select' aria-label='Default select example' name='estado' id='estado'><option selected>elige una opción.....</option><option class='opcion_est' value='pendiente'>Pendiente</option><option value='Completado'>Completado</option><option value='cancelado'>Cancelado</option></select></td>";
 
         var btn = document.createElement("TR");
         btn.innerHTML=fila;
@@ -26,10 +26,11 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#btnAgregarItem').click(function() {
       guardar();
       cambiarColor();
+      
       $("#ModalAgregar").modal('hide');
       
     });
-
+    ColorEstado();
 //permite limpiar los campos del modal agregar tarea, para que no muestre contenido utilizado
     function LimpiarFormulario() {
       $('#tarea').val('');
@@ -50,9 +51,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 td[i].style.backgroundColor = 'orange';
             }
         }
+    }
+    
+
+        function ColorEstado(){
+            let td = document.getElementsByClassName('opcion_est');
+            for(let i = 0; i < td.length; i++){
+                if(td[i].innerHTML === 'Pendiente'){
+                    td[i].style.backgroundColor = 'yellow';
+                }else if(td[i].innerHTML === 'Completado'){
+                    td[i].style.backgroundColor = 'green';
+                    td[i].style.color = 'black';
+                }else if(td[i].innerHTML === 'Cancelado'){
+                    td[i].style.backgroundColor = 'Red';
+                }
+            } 
 
     }
 
-
-
-  });
+});
